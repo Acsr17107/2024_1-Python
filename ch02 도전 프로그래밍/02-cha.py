94eca5ebcb67e880731c5ea0f5fc5b401b352ed2
Acsr17107 <sj060700@naver.com>
2024-09-05 19:44:57 +0900

    Delete ch02 도전 프로그래밍 directory

--- ch02 도전 프로그래밍/02-cha.py
+++
@@ -1,3 +0,0 @@
-km = int(input('차의 속도를 입력(km) >> '))
-miles = km / 1.61
-print(str(km) + '(km)은', miles, '마일(miles)이다.')
--- ch02 도전 프로그래밍/04-cha.py
+++
@@ -1,7 +0,0 @@
-Eradius = 40120
-r = 6378.1
-Cradius = 2 * 3.141592 * r
-dif = Eradius - Cradius
-print('알려진 지구 둘레:', Eradius)
-print('지구와 같은 원둘레:', Cradius)
-print('차이:', str(dif) + '(km)')
