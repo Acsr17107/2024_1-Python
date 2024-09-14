291148c6820d9999ae7224f75906048780df5700
Acsr17107 <sj060700@naver.com>
2024-09-14 21:29:54 +0900

    Add files via upload

---
+++ ch02/02-02comments.py
@@ -0,0 +1,7 @@
+''' 01-02comments.py
+    2019 3. by kang Hwan Soo'''
+
+print('# 이후는 주석') # 한 줄에서 문장 이후에도 주석 사용 가능
+print('string: "python"') # 큰따옴표 내부에서 작은 따옴표는 문자열
+print("number: 1 5 3.14") # 문자열 내부에서 숫자도 문자열
+print("string: 'python'") # 작은따옴표 내부에서 작은따옴표는 문자열
