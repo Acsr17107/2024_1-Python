1bce863f45f6fd29c42b6ee105a734d0bc7e54e4
Neon <sj060700@naver.com>
2024-09-30 02:29:22 +0900

    Delete ch03 Challenge directory

--- ch03 Challenge/03-cha.py
+++
@@ -1,5 +0,0 @@
-str = 'Beautiful is better than ugly.'
-print(str)
-a = str.replace('Beautiful', 'Explicit')
-b = a.replace('ugly', 'implicit')
-print(b)
--- ch03 Challenge/04-cha.py
+++
@@ -1,6 +0,0 @@
-url = 'https://docs.python.org/3/tutorial'
-a = url.replace(':', '')
-b = a.replace('/', '')
-print(b[0:b.find('d')])
-print(b[b.find('d'):b.find('3')])
-print(b[b.find('3') + 1:])
