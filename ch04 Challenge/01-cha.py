cec7a7b79e32614ead5583206f14ab148febe88c
Neon <sj060700@naver.com>
2024-10-01 12:22:04 +0900

    Delete ch04 Challenge directory

--- ch04 Challenge/01-cha.py
+++
@@ -1,9 +0,0 @@
-m = int(input('월 입력 ? '))
-if m == 4 or m == 5:
-    print(str(m) + '월 봄')
-elif m == 6 or m == 7 or m == 8:
-    print(str(m) + '월 여름')
-elif m == 9 or m == 10:
-    print(str(m) + '월 가을')
-elif m == 11 or m == 12 or m == 1 or m == 2 or m == 3:
-    print(str(m) + '월 겨울')
--- ch04 Challenge/03-cha.py
+++
@@ -1,5 +0,0 @@
-from random import randint
-x = randint(1, 99)
-y = randint(1, 99)
-z = randint(1, 99)
-print(x, y, z, '중에서 최대:', max(x, y, z))
