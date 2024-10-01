90e7ed88aa6b888eaf8f48dd5ca943540067f713
Neon <sj060700@naver.com>
2024-10-01 12:26:50 +0900

    Delete ch07 Challenge directory

--- ch07 Challenge/02-cha.py
+++
@@ -1,9 +0,0 @@
-def getintrest(m, r, y):
-    a = m * (1 + r) ** y
-    print('{}년 총액: {:.2f}'.format(y, a))
-
-print('예금 원금:', 300000)
-getintrest(300000, 0.05, 2)
-getintrest(300000, 0.05, 4)
-getintrest(300000, 0.05, 6)
-getintrest(300000, 0.05, 8)
