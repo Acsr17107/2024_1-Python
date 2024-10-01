24af5337b4977b8189f9fd93930e4000392a388a
Neon <sj060700@naver.com>
2024-10-02 00:21:54 +0900

    Delete ch11 directory

--- ch11/11-01basictryexcept.py
+++
@@ -1,5 +0,0 @@
-try:
-    data = int('3.67')
-except Exception as e:
-    print('예외 발생 이름: {}'.format(type(e)))
-    print('예외 발생 이유: {}'.format(e))
--- ch11/11-07readlinefile.py
+++
@@ -1,9 +0,0 @@
-f = open('pyzen.txt', 'r')
-
-while True:
-    line = f.readline()
-    if not line: break
-    print(line, end = '')
-    # print(line.strip('\n'))
-
-f.close()
