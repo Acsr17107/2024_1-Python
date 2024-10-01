f3f9df8a60b6b0aca44aedeb31934b7c7468b692
Neon <sj060700@naver.com>
2024-10-01 12:23:14 +0900

    Delete ch05 Challenge directory

--- ch05 Challenge/01-cha.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-lst = []
-for i in range(10):
-    a = randint(1, 99)
-    lst.append(a)
-print('리스트', lst)
-print('정렬 리스트', sorted(lst))
-print('역순 리스트', sorted(lst, reverse=1))
