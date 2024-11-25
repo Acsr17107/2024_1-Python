89a95dfc8ef7ceecab62e1f645ba8b7b354fcb98
Neon <sj060700@naver.com>
2024-11-25 20:27:04 +0900

    Delete ch06_Challenge directory

--- ch06_Challenge/05-cha.py
+++
@@ -1,7 +0,0 @@
-fruits = ['apple', 'banana', 'grapes', 'pear']
-prices = (1000, 500, 1200, 1500)
-dic = dict(zip(fruits, prices))
-print(dic); print()
-
-for i, j in enumerate(dic, start=1):
-    print('{} {} 가격: {}'.format(i, j, prices[i - 1]))
