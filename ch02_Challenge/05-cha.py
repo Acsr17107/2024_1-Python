b6d633b26af0c08c3049c17775f672d9ab5eeefc
Neon <sj060700@naver.com>
2024-11-25 20:26:14 +0900

    Delete ch02_Challenge directory

--- ch02_Challenge/05-cha.py
+++
@@ -1,7 +0,0 @@
-c = int(input('온도 입력 >> '))
-fahrenhite = c * 9/5 + 32
-f = c * 2 + 30
-dif = fahrenhite - f
-print('정확 계산: 섭씨:', c , ',', '화씨:', fahrenhite)
-print('약식 계산: 섭씨:', c , ',', '화씨:', f)
-print('차이:', dif)
