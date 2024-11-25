94c193afc67d4020ebf0df6f5980f1d5b99c4506
Neon <sj060700@naver.com>
2024-11-25 20:26:32 +0900

    Delete ch04 directory

--- ch04/04-01rideif.py
+++
@@ -1,3 +0,0 @@
-heigth = 152 # 탑승을 체크할 키를 대입
-if 140 <= heigth:
-    print('롤러코스터 T=Express, 즐기세요!')
--- ch04/04-14menubreak.py
+++
@@ -1,5 +0,0 @@
-while True:
-    menu = input('[0]종료 [1]계속 ? ')
-    if menu == '0':
-        break
-print('종료')
