72eaeba4d6a805716eaaabec601c90465b972da8
Neon <sj060700@naver.com>
2024-11-25 20:33:27 +0900

    Delete ch09 directory

--- ch09/09-04importkutil.py
+++
@@ -1,6 +0,0 @@
-from kutil import nrandom
-
-for i in range(5):
-    print('로또 복권 %d:' % (i+1), nrandom(1, 45, 6))
-print()
-print('주사위 4번: ', nrandom(1, 6, 4, True))
--- ch09/09-10matplotlib.py
+++
@@ -1,4 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.plot([2, 4, 6, 8], [8, 10, 3, 6])
-plt.show()
