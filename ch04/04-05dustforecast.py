488efd308d01f3cef1ef9910d2986fcfa89fb0fb
Neon <sj060700@naver.com>
2024-12-30 22:45:08 +0900

    Add files via upload

---
+++ ch04/04-05dustforecast.py
@@ -0,0 +1,10 @@
+PM = float(input('미세먼지의 농도는 ? '))
+
+if 151 <= PM:
+    print('미세먼지 농도: {:.2f}, 등급: {}'.format(PM, '매우 나쁨'))
+elif 81 <= PM:
+    print('미세먼지 농도: {:.2f}, 등급: {}'.format(PM, '나쁨'))
+elif 31 <= PM:
+    print('미세먼지 농도: {:.2f}, 등급: {}'.format(PM, '보통'))
+else:
+    print('미세먼지 농도: {:.2f}, 등급: {}'.format(PM, '좋음'))
